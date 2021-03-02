72cbe81d1f1f86ce4bf021b3df69ef60666145a4
Vlad Spatariu <vlad.spatariu@gmail.com>
2021-03-02 20:02:03 +0200

    Cleaned up code a little

--- assets/js/main.js
+++ assets/js/main.js
@@ -102,46 +102,12 @@
     $(".mobile-nav, .mobile-nav-toggle").hide();
   }
 
-  // Torn out function so that code passes only once
+  // Move logo depending on window size
         if ($(window).width() < 991) {
-          //if the window is greater than 440px wide then turn on jScrollPane..
-        console.log('is Mobile');
-        console.log($(window).width());
         $(".main-logo").insertBefore('.mainPageContent');
       }
       else {
         $(".main-logo").insertAfter('.mainPageContent');
-        console.log('is Desktop');
       }
-
-
-
-  // jQuery counterUp
-  // $('[data-toggle="counter-up"]').counterUp({
-  //   delay: 10,
-  //   time: 1000
-  // });
-
-  // var $window = $(window);
-  // var $pane = $('#pane1');
-
-  // function checkWidth() {
-  //     var windowsize = $window.width();
-  //     if (windowsize < 991) {
-  //         //if the window is greater than 440px wide then turn on jScrollPane..
-  //       console.log('is Mobile');
-  //       console.log($window.width());
-  //       $(".main-logo").insertBefore('.mainPageContent');
-  //     }
-  //     else {
-  //       $(".main-logo").insertAfter('.mainPageContent');
-  //       console.log('is Desktop');
-  //     }
-  // }
-  // // Execute on load
-  // checkWidth();
-  // // Bind event listener
-  // $(window).resize(checkWidth);
-
-
+      
 })(jQuery);
